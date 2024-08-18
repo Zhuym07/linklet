c5a667d0cb0f7442e41f1d65f278f99d970fee1b
Zhuym07 <34023479+Zhuym07@users.noreply.github.com>
2024-08-18 13:56:49 +0800

    fix

--- functions/[id].js
+++ functions/[id].js
@@ -1,38 +1,50 @@
-const timedata = new Date();
-const formattedDate = new Intl.DateTimeFormat('zh-CN', options).format(timedata);
+/**
+ * @param {string} slug
+ */
+import page404 from './404.html'
 
-const slug = params.id;
+export async function onRequestGet(context) {
+    const { request, env, params } = context;
+    // const url = new URL(request.url);
+    const clientIP = request.headers.get("x-forwarded-for") || request.headers.get("clientIP");
+    const userAgent = request.headers.get("user-agent");
+    const Referer = request.headers.get('Referer') || "Referer"
+    const originurl = new URL(request.url);
+    const options = {
+        timeZone: 'Asia/Shanghai',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour12: false,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    const timedata = new Date();
+    const formattedDate = new Intl.DateTimeFormat('zh-CN', options).format(timedata);
 
-const Url = await env.DB.prepare(`SELECT url FROM links where slug = '${slug}'`).first();
+    const slug = params.id;
 
-if (!Url) {
-    return new Response(page404, {
-        status: 404,
-        headers: {
-            "content-type": "text/html;charset=UTF-8",
+    const Url = await env.DB.prepare(`SELECT url FROM links where slug = '${slug}'`).first()
+
+    if (!Url) {
+        return new Response(page404, {
+            status: 404,
+            headers: {
+                "content-type": "text/html;charset=UTF-8",
+            }
+        });
+    } else {
+        try {
+            const info = await env.DB.prepare(`INSERT INTO logs (url, slug, ip,referer,  ua, create_time) 
+            VALUES ('${Url.url}', '${slug}', '${clientIP}','${Referer}', '${userAgent}', '${formattedDate}')`).run()
+            // console.log(info);
+            return Response.redirect(Url.url, 302);
+            
+        } catch (error) {
+            console.log(error);
+            return Response.redirect(Url.url, 302);
         }
-    });
-} else {
-    try {
-        // 检查 Referer 是否为 domain.com/js/click2short.js
-        if (Referer && Referer.includes('domain.com/js/click2short.js')) {
-            const click2shortJs = await fetch('https://example.com/asset/js/click2short.js');
-            const jsContent = await click2shortJs.text();
-            return new Response(jsContent, {
-                status: 200,
-                headers: {
-                    "content-type": "application/javascript;charset=UTF-8",
-                }
-            });
-        }
-
-        const info = await env.DB.prepare(`INSERT INTO logs (url, slug, ip, referer, ua, create_time) 
-        VALUES ('${Url.url}', '${slug}', '${clientIP}', '${Referer}', '${userAgent}', '${formattedDate}')`).run();
-        // console.log(info);
-        return Response.redirect(Url.url, 302);
-
-    } catch (error) {
-        console.log(error);
-        return Response.redirect(Url.url, 302);
     }
+
 }
